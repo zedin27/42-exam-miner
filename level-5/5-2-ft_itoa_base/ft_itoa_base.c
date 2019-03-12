53cb1d982e89c9916dc316b788b1adb90a12d6d9
zedin27 <ztisneshamzeh@csumb.edu>
2019-03-11 23:58:02 -0700

    WIP

--- level-5/5-2-ft_itoa_base/ft_itoa_base.c
+++ level-5/5-2-ft_itoa_base/ft_itoa_base.c
@@ -6,14 +6,78 @@
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/03 22:36:30 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/08 17:20:30 by angavrel         ###   ########.fr       */
+/*   Updated: 2019/03/11 23:57:49 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h> //malloc protection isn't required for the exam
 
-char	*ft_itoa_base(int value, int base)
+int			ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char		ft_strcpy(char *dst, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (dst);
+}
+
+
+
+void		ft_strdel(char **str)
+{
+	if (str)
+	{
+		free(*as);
+		*as = NULL;
+	}
+}
+
+static int	len_nbr_base(int value, int base)
+{
+	int len;
+
+	len = 0;
+	while (value)
+	{
+		len++;
+		value /= base;
+	}
+	return (len);
+}
+
+static char	*negative_sign(int n, int base, char *ptr)
+{
+	if (n < 0 && base == 10)
+		ptr[0] = '-';
+	return (ptr);
+}
+
+char	*ft_itoa_base(int n, int base)
+{
+	int len;
+	long value;
+	char *ptr;
+	char *b;
+
+	len = len_nbr_base(n, base);
+	value = n;
+	if (!(b = 
+}
+
+/*char	*ft_itoa_base(int value, int base)
 {
 	char	*s;
 	long	n;
@@ -43,3 +107,4 @@ int		main(int ac, char **av)//
 		printf("%s", ft_itoa_base(atoi(av[1]), atoi(av[2])));//
 	return (1);//
 }//
+*/
